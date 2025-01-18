23dc926cd38a0e5eb07993fd76bc5a877a8b42cd
FRC Team 1764 <first1764@gmail.com>
2025-01-18 10:39:51 -0600

    2025 basic working code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,86 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+  public PWMTalonSRX frontRight;
+  public PWMTalonSRX frontLeft;
+  public PWMTalonSRX backLeft;
+  public PWMTalonSRX backRight;
+  public XboxController controller;
+  public Joystick joystick;
+  public final double deadzone = 0.07;
+  public final double maxspeed = 0.5;
+
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  public Robot() {
+    controller = new XboxController(0);
+  
+    frontRight = new PWMTalonSRX(0); 
+    frontLeft = new PWMTalonSRX(7);
+    backRight = new PWMTalonSRX(8);
+    backLeft = new PWMTalonSRX(9);
+
+    frontRight.addFollower(backRight);
+    frontLeft.addFollower(backLeft);
+    frontLeft.setInverted(true);
+
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    double leftPower = controller.getLeftY() + arcadeDrive.getX(controller.getLeftX(), true);
+    double rightPower = controller.getLeftY() + arcadeDrive.getX(controller.getLeftX(), false);
+    leftPower = (Math.abs(leftPower) < deadzone)? 0 : leftPower;
+    rightPower = (Math.abs(rightPower) < deadzone)? 0 : rightPower;
+    frontLeft.set(leftPower*maxspeed);
+    frontRight.set(rightPower*maxspeed);
+
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+
+
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+}
---
+++ src/main/java/frc/robot/arcadeDrive.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class arcadeDrive extends SubsystemBase {
+  /** Creates a new arcadeDrive. */
+  public arcadeDrive() {
+
+  }
+
+  public static double getX(double x, boolean left) {
+    boolean giveback = false;
+    if (x<0 && !left) {
+      giveback = true;
+      x*=1;
+    } else if (x>0 && left) {
+      giveback = true;
+      x*=-1;
+    } else {
+      x = 0;
+    }
+    if (!giveback) {
+        x = 0;
+    }
+    return x;
+  }
+
+  public static double getXWeird(double x) {
+    double newX = 0;
+    double neg;
+    
+    if (x<0) {
+      neg = -1;
+    } else {
+      neg = 1;
+    }
+
+    x*=neg;
+    x/=2;
+
+    return newX;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
